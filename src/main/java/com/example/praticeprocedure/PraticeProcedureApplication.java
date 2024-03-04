2c885c6e362fddfeb0c727a8b2554c5e63fb3bfc
Min <74136649+DANGHYEON@users.noreply.github.com>
2024-03-05 04:38:15 +0900

    초기세팅

---
+++ src/main/java/com/example/praticeprocedure/PraticeProcedureApplication.java
@@ -0,0 +1,13 @@
+package com.example.praticeprocedure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticeProcedureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PraticeProcedureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/praticeprocedure/PraticeProcedureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.praticeprocedure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticeProcedureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
